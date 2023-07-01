2d64bcc7945cec327d61e0c382ba283595c1cdb4
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-06-30 19:16:00 -0500

    🎨 Standardize naming (ProUI) (#25982)

--- Marlin/src/gcode/sd/M1001.cpp
+++ Marlin/src/gcode/sd/M1001.cpp
@@ -55,8 +55,6 @@ extern MarlinUI ui;
 
 #if ENABLED(EXTENSIBLE_UI)
   #include "../../lcd/extui/ui_api.h"
-#elif ENABLED(DWIN_LCD_PROUI)
-  #include "../../lcd/e3v2/proui/dwin.h"
 #endif
 
 #if ENABLED(HOST_ACTION_COMMANDS)
