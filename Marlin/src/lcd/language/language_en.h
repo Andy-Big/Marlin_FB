59aecff12705584d44e52a028eacc0e08575c98f
AndyBig <andybig@gmail.com>
2023-07-09 23:22:48 +0300

    Fix language_en

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -939,13 +939,10 @@ namespace Language_en {
 
 
   // These strings can be the same in all languages
-  LSTR MSG_MARLIN                         = _UxGT("MarlinRB");
+  LSTR MSG_MARLIN                         = _UxGT("Marlin");
   LSTR MSG_SHORT_DAY                      = _UxGT("d"); // One character only
   LSTR MSG_SHORT_HOUR                     = _UxGT("h"); // One character only
   LSTR MSG_SHORT_MINUTE                   = _UxGT("m"); // One character only
-
-  // These strings can be the same in all languages
-  LSTR MSG_MARLIN                         = _UxGT("Marlin");
   LSTR MSG_PID_P                          = _UxGT("PID-P");
   LSTR MSG_PID_P_E                        = _UxGT("PID-P *");
   LSTR MSG_PID_I                          = _UxGT("PID-I");
