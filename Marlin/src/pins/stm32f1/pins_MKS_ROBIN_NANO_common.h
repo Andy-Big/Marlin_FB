MarlinFirmware/Marlin

🎨 Rename FAN_PIN to FAN0_PIN (#25568)

Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
@@ -105,8 +105,8 @@
 #ifndef HEATER_0_PIN
   #define HEATER_0_PIN                      PC3
 #endif
-#ifndef FAN_PIN
-  #define FAN_PIN                           PB1   // FAN
+#ifndef FAN0_PIN
+  #define FAN0_PIN                          PB1   // FAN
 #endif
 #ifndef HEATER_BED_PIN
   #define HEATER_BED_PIN                    PA0

Andy-Big/Marlin_FB

Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_common.h
@@ -124,8 +124,8 @@
     #define HEATER_1_PIN                    PB0
   #endif
 #endif
-#ifndef FAN_PIN
-  #define FAN_PIN                           PB1   // FAN
+#ifndef FAN0_PIN
+  #define FAN0_PIN                           PB1   // FAN0
 #endif
 #ifndef HEATER_BED_PIN
   #define HEATER_BED_PIN                    PA0